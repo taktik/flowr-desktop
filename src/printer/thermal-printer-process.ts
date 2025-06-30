e59d79d6344f9770fca91c0a4e9e4198b4c8f271
PME_STATION_3\merte <pme@taktik.com>
2025-06-30 16:24:04 +0200

    wip

--- src/printer/thermal-printer-process.ts
+++ src/printer/thermal-printer-process.ts
@@ -16,12 +16,34 @@ export class ThermalPrinterProcess {
         this.instance = undefined;
     }
 
+    private async getPrinterData(printerName: string) {
+        const printers = await this.webContents.getPrintersAsync()
+        return printers.find((p) => p.name === printerName)
+    }
+
+    private async getPrinter(_: unknown, printerName: string) {
+        try {
+            console.log('printerName', printerName)
+            const printer = await this.getPrinterData(printerName)
+            console.log('printer', printer)
+            this.webContents.send('printer-data', printer)
+        } catch (err) {
+            this.webContents.send('get-printer-error', {
+                errorMessage: 'Error getting printer data'
+            })
+        }
+    }
+
     private async printContent(_: unknown, param: {data: PosPrintData[], options: PosPrintOptions}) {
         try {
-            const result = await PosPrinter.print(param.data, param.options)
-            this.webContents.send('print-success', {
-                result
-            })
+            const printer = await this.getPrinterData(param.options.printerName)
+            if (printer.status !== 0) {
+                this.webContents.send('print-error', {
+                    errorMessage: 'Printer not connected/ready'
+                })
+            }
+             await PosPrinter.print(param.data, param.options)
+            this.webContents.send('print-success')
 
         } catch (err) {
             this.webContents.send(`print-error`, {
@@ -32,6 +54,7 @@ export class ThermalPrinterProcess {
     }
     private startListeningEvents() {
         ipcMain.on('print-content', this.printContent.bind(this))
+        ipcMain.on('get-printer-info', this.getPrinter.bind(this))
     }
 
     private constructor(webContents: WebContents) {
